92416c6cb7a8f2e04b48a1d67364bd56d2e1ab29
arishilyaeva <arina.shilyaeva@actionengine.com>
2022-01-19 16:47:14 +0300

    remove unused contracts

--- contracts/adapters/interfaces/IUniswapV2Router.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.8.0;
-
-// SPDX-License-Identifier: MIT
-
-import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
-
-
-interface IUniswapV2Router is IUniswapV2Router02 {
-}
